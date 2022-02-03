c4c2b6839cae70d5c4c595110cf9ab8b7305f23d
Matt Young <mattyoung@Matts-MacBook-Pro-3.local>
2022-02-03 13:10:15 -0500

    Further unnest data

--- weatherXC.py
+++ weatherXC.py
@@ -15,10 +15,9 @@ df_list = []
 city = ['Tallahasee','Stillwater','Terra Haute','Madison','Louisville']
 zipcodes = ['32311', '74075', '47801','53593','40241']
 dates = ['2021-11-20','2021-03-15','2019-11-23','2018-11-17','2017-11-18']
-years = ['2021','2020','2019','2018','2017']
 
 #Collect weather data on XC nationals and compare with previous week rankings
-for x, y in zip(zipcodes,dates):
+for x, y, z in zip(zipcodes,dates,city):
     weather_key = '7feb2c95e6fd45dfbc653851220302'
     BaseURL = 'http://api.worldweatheronline.com/premium/v1/past-weather.ashx'
     URLPost = {'key': weather_key,
@@ -27,16 +26,16 @@ for x, y in zip(zipcodes,dates):
            'format' : 'json'}
     r2= requests.get(BaseURL, URLPost) #pull information combining both BaseURL and URLPost
     out = json.loads(r2.text) #Load the data into json
-    clean = json_normalize(out, record_path = ['data','weather'])#Normalize the data
+    clean = json_normalize(out, record_path = ['data','weather','hourly'], meta = [['data','weather','maxtempF'],['data','weather','mintempF'],['data','weather','avgtempF'],['data','weather','totalSnow_cm'],['data','weather','sunHour'],['data','weather','uvIndex']])#Normalize the data
     zipDF = pd.DataFrame(clean) #transform data to DF
+    zipDF['zipCode'] = x
+    zipDF['City'] = z
     df_list.append(zipDF)
 
 weather = pd.concat(df_list)
 
-weather['Year'] = years
-weather['zipCode'] = zipcodes
-weather['city'] = city
 
-weather = weather.drop(columns={'astronomy','hourly'})
+weather = weather.drop(columns={'weatherIconUrl'})
 
 weather.to_csv('weather_xc.csv', index=False)
+
